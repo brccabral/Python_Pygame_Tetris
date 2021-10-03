f7456af5fba2b3281592eb638d8ae373c148faba
Bruno Cabral <cabral.brc@gmail.com>
2021-10-03 09:24:58 -0700

    check for change_piece

--- tetris.py
+++ tetris.py
@@ -298,6 +298,14 @@ def main(win):
             if y > -1:
                 grid[y][x] = current_piece.color
         
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_positions[p] = current_piece.color
+            current_piece = next_piece
+            next_piece = get_shape()
+            change_piece = False
+        
         draw_window(win, grid)
 
 def main_menu(win):
